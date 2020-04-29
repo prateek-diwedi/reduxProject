d6f026987cc1a3b660bdb5caf48f6f3c92877124
Prateek Diwedi <diwedi1112@gmail.com>
2020-04-29 13:46:28 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// Action Increment
+const increment = () => {
+    return {
+      type: 'INCREMENT'
+    }
+} 
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+} 
+
+// Reducer 
+const counter = (state=0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state -1;
+  }
+}
+
+let store = createStore(counter)
+
+//console log
+store.subscribe(() => console.log(store.getState()))
+
+// DISPATCH
+store.dispatch(increment());
+
+
 
 ReactDOM.render(
   <React.StrictMode>
